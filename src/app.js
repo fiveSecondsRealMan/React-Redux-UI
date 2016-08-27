7538500f2f43170669663bdb1c129a5d6c10f901
root <root@zengwenbin.(none)>
2016-08-27 08:14:49 +0800

    Button基础部件完成

--- src/app.js
+++ src/app.js
@@ -1,1 +1,1 @@
-console.log('ass');
+console.log('assdsfds111');
--- src/components/widgets/Button.js
+++ src/components/widgets/Button.js
@@ -10,15 +10,47 @@ import 'styles/button';
 
 class Button extends Component {
   disable() {
-
+		const { disableAction } = { this.props.action };
+		disableAction.apply(this, true);
   }
 
-  agentRequest() {
+	enable(...args) {
+		const { enableAction } = { this.props.action };
+		enableAction.apply(this, false);
+	}
 
+  fetch() {
+		this.props.eventHandle();
   }
+
+	interactive() {
+		this.props.eventHandle();
+	}
+
+	handleClick() {
+		if (this.multied) {
+		  this.fetch();
+			this.disable();
+		} else {
+			this.interactive();			
+		}
+	}
 
   render() {
-    
+		const { type, className, style, children, disabled } = this.props;
+
+		return (
+			<button type={ type } 
+				className={ className }  
+				style={ style }
+				onClick={ this.handleClick  }
+				disabled={ disabled }>
+
+		  { children  }			
+
+			</button> 
+		);
+				 
   }
 }
 
@@ -26,13 +58,14 @@ class Button extends Component {
   组件接收的props
 **/
 Button.propTypes = {
-  action: PropTypes.object,               // 组件发生交互动作
-  children: PropTypes.string,             // 显示信息
-  className: PropTypes.string,            // class名
-  disabled: PropTypes.bool,               // 是否禁用
-  eventName: PropTypes.string,            // 事件名
-  eventHandle: PropTypes.func,            // 事件处理程序
-  style: PropTypes.object                 // css样式
+  action: PropTypes.object,               										 // 组件发生交互的动作
+  children: PropTypes.string,             										 // 显示信息
+  className: PropTypes.string,            										 // class名
+	style: PropTypes.object,																		 // 样式对象
+ 	type: PropTypes.oneOf(['submit', 'button']),                 // 按钮类型
+	eventHandle: PropTypes.func,            										 // 事件处理程序
+  disabled: PropTypes.bool,               										 // 是否禁用
+	multied: PropTypes.bool																		   // 是否多次点击
 };
 
 export default Button;
